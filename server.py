769760ac46fee665ffc3e09b7a99367f0f3363d5
Tranzorro <shiekadam@gmail.com>
2021-10-24 21:40:03 -0400

    making and showing users

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, request
+from user import User
+app = Flask(__name__)
+@app.route('/')
+def default():
+    users = User.get_all()
+    print(users)
+    return render_template("users.html", all_users = users)
+
+@app.route('/new')
+def newUser():
+    return render_template("new.html")
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    User.save(data)
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
